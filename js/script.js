fa03e786e597c21bad772b43ce74ae6e6e5aa32e
27SARRAZIN <sarrazinr5@gmail.com>
2025-01-14 17:42:42 -0400

    finalizando menu responsivo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function menuDropdow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu-icon.png";
+    }else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/icons.png";
+    }
+}
